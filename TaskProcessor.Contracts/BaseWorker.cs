844eea837a8b4821da50bb0844155f4060843811
Martin Lantzsch <martin@linux-doku.de>
2014-05-05 22:51:31 +0200

    removed nuget hack from travis file. Fixed #3

---
+++ TaskProcessor.Contracts/BaseWorker.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskProcessor.Contracts
+{
+	public abstract class BaseWorker : IWorker
+	{
+		ITaskQueue Queue { get; }
+
+		public BaseWorker(ITaskQueue queue)
+		{
+			Queue = queue;
+		}
+
+		abstract public void Start();
+		abstract public void Stop();
+		abstract public void Cancel();
+		abstract public void AbortCancel();
+	}
+}
