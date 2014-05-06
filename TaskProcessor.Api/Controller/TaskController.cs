5b9429d4b167077a9fa3d4b5614f408b2e4559e3
Martin Lantzsch <martin@linux-doku.de>
2014-05-06 22:49:07 +0200

    Added new working Api Project (WebApi2 via Owin self host)

---
+++ TaskProcessor.Api/Controller/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace TaskProcessor.Api.Controller
+{
+	public class TaskController : ApiController
+	{
+		public string Get(int id) {
+			return "Hello World";
+		}
+	}
+}
+
---
+++ TaskProcessor.Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace TaskProcessor.Api
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string baseAddress = "http://localhost:9000/";
+
+			WebApp.Start<Startup>(url: baseAddress);
+
+			Console.ReadLine();
+		}
+	}
+}
