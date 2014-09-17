5c715c52e8f6d3bc95290481e6d6d6a18557d6ce
Martin Lantzsch <martin@linux-doku.de>
2014-09-17 14:11:30 +0200

    config refactoring

--- TaskProcessor/Configuration/JsonConfiguration.cs
+++ TaskProcessor/Configuration/JsonConfiguration.cs
@@ -1,23 +1,65 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
-using TaskProcessor.Contracts;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json.Linq;
+using TaskProcessor.Contracts;
 
 namespace TaskProcessor.Configuration
 {
     public class JsonConfiguration : IConfiguration
     {
         private int _workers;
-        private IEnumerable<ITask> _tasks;
+        private IList<ITask> _tasks;
 
         public JsonConfiguration(string jsonString)
         {
             var source = JObject.Parse(jsonString);
             if (source != null) { 
                 _workers = (int)source.SelectToken("workers");
-                _tasks = Enumerable.Empty<ITask>();
+                _tasks = new List<ITask>();
+
+                foreach (var taskConfig in source.SelectToken("tasks"))
+                {
+                    ITask task = null;
+
+                    var assemblyName = (string)taskConfig.SelectToken("assembly");
+                    var className = (string)taskConfig.SelectToken("class");
+                    var argumentList = taskConfig.SelectToken("arguments");
+
+                    var typeName = className;
+                    if (assemblyName != null)
+                    {
+                        typeName = className + "," + assemblyName;
+                    }
+
+                    var type = Type.GetType(typeName);
+                    foreach (var constructor in type.GetConstructors())
+                    {
+                        var parameters = constructor.GetParameters();
+                        if (parameters.Length == argumentList.Count())
+                        {
+                            // try with this constructor
+                            try
+                            {
+                                var arguments = new List<Object>();
+
+                                for (var i = 0; i < parameters.Length; i++)
+                                {
+                                    arguments.Add(argumentList[i].ToObject(parameters[i].ParameterType));
+                                }
+
+                                task = (ITask)Activator.CreateInstance(type, arguments.ToArray());
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+                        }
+
+                        _tasks.Add(task);
+                    }
+                }
             }
         }
 
@@ -28,7 +70,7 @@ namespace TaskProcessor.Configuration
             get { return _workers; }
         }
 
-        public System.Collections.Generic.IEnumerable<TaskProcessor.Contracts.ITask> Tasks
+        public IEnumerable<ITask> Tasks
         {
             get { return _tasks; }
         }
--- TaskProcessor/Program.cs
+++ TaskProcessor/Program.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using Newtonsoft.Json.Linq;
 using Microsoft.Owin.Hosting;
 using System.Net.Sockets;
+using TaskProcessor.Configuration;
 
 namespace TaskProcessor
 {
@@ -21,12 +22,12 @@ namespace TaskProcessor
 			var configFile = "./config.json";
 
 			if(File.Exists(configFile)) {
-				var configFileText = File.ReadAllText(configFile);
-				var config = JObject.Parse(configFileText);
+                var configFileText = File.ReadAllText(configFile);
+                var config = new JsonConfiguration(configFileText);
 
 				if(config != null) {
 					// start workeres
-					var workers = new Action[(int)config.SelectToken("workers")];
+					var workers = new Action[config.Workers];
 
 					if(workers.Length < 1) {
 						Console.WriteLine("Please add more than one worker to the config!");
@@ -41,44 +42,9 @@ namespace TaskProcessor
                     //Parallel.Invoke(workers);
 
 					// add tasks to queue
-					foreach(var taskConfig in config.SelectToken("tasks")) {
-						ITask task = null;
-
-						var assemblyName = (string)taskConfig.SelectToken("assembly");
-						var className = (string)taskConfig.SelectToken("class");
-						var argumentList = taskConfig.SelectToken("arguments");
-
-						var typeName = className;
-						if(assemblyName != null) {
-							typeName = className + "," + assemblyName;
-						}
-
-						var type = Type.GetType(typeName);
-						foreach(var constructor in type.GetConstructors()) {
-							var parameters = constructor.GetParameters();
-							if(parameters.Length == argumentList.Count()) {
-								// try with this constructor
-								try {
-									var arguments = new List<Object>();
-
-									for(var i = 0; i < parameters.Length; i++) {
-										arguments.Add(argumentList[i].ToObject(parameters[i].ParameterType));
-									}
-										
-									task = (ITask)Activator.CreateInstance(type, arguments.ToArray());
-									break;
-								} catch {
-									continue;
-								}
-							}
-						}
-
-						if(task != null) {
-                            var taskExecution = new TaskExecution(task);
-                            queue.Add(taskExecution);
-						} else {
-							Console.WriteLine("No Task or suiteable constructor found! Task Name: '" + task.Name + "'");
-						}
+					foreach(var task in config.Tasks) {
+                        var taskExecution = new TaskExecution(task);
+                        queue.Add(taskExecution);
 					}
 				}
 			} else {
