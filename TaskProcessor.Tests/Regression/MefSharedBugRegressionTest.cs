61cd4cdd75331dc8f24aea72fcf3f59893b02bb9
Martin Lantzsch <martin@linux-doku.de>
2014-10-08 21:32:58 +0200

    wip regression test for #8

---
+++ TaskProcessor.Tests/Regression/MefSharedBugRegressionTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Composition;
+using System.Composition.Hosting;
+
+namespace TaskProcessor.Tests.Regression
+{
+    [TestFixture]
+    public class MefSharedBugRegressionTest
+    {
+        [Shared]
+        private class SharedManager
+        {
+            private int _counter = 0;
+
+            public int GetCounter()
+            {
+                return ++_counter;
+            }
+        }
+
+        [Test]
+        public void SharedTest()
+        {
+            var containerConfiguration = new ContainerConfiguration();
+            containerConfiguration.WithPart<SharedManager>();
+
+            var container = containerConfiguration.CreateContainer();
+
+            Assert.AreSame(1, container.GetExport<SharedManager>().GetCounter());
+        }
+    }
+}
