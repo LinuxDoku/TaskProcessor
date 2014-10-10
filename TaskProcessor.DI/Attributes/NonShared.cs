538edaa0d2dd479dce76812840e45e211cce2502
Martin Lantzsch <martin@linux-doku.de>
2014-10-10 21:43:58 +0200

    missing di files

---
+++ TaskProcessor.DI/Attributes/NonShared.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProcessor.DI.Attributes {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class NonShared : Attribute {
+    }
+}
---
+++ TaskProcessor.DI/Container.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using TaskProcessor.DI.Attributes;
+
+namespace TaskProcessor.DI {
+    public class Container {
+        private static Container _instance = new Container();
+        private readonly IContainer _container;
+
+        private delegate Type TypeOfExport(Type type);
+        private TypeOfExport _defaultTypeOfExport;
+
+        private Container() {
+            _defaultTypeOfExport = x => x.GetCustomAttributes(typeof (Export), true).OfType<Export>().First().Type ?? x;
+
+            var containerBuilder = new ContainerBuilder();
+
+            // load current assembly
+            RegisterAssembly(containerBuilder, Assembly.GetExecutingAssembly());
+
+            _container = containerBuilder.Build();
+        }
+
+        public static T GetExport<T>() {
+            return _instance._container.Resolve<T>();
+        }
+
+        public static object GetExport(Type type) {
+            object result;
+            if (_instance._container.TryResolve(type, out result)) {
+                return result;
+            }
+            return null;
+        }
+
+        public static void RegisterAssembly(Assembly assembly) {
+            if (assembly == null) return;
+
+            var containerBuilder = new ContainerBuilder();
+            _instance.RegisterAssembly(containerBuilder, assembly);
+            containerBuilder.Update(_instance._container);
+        }
+
+        private void RegisterAssembly(ContainerBuilder builder, Assembly assembly) {
+            var types = assembly.GetTypes()
+                                .Where(x => x.GetCustomAttributes(typeof (Export), true).Any())
+                                .ToArray();
+
+            builder.RegisterTypes(types)
+                .Where(x => x.GetCustomAttributes(typeof (Shared), true).Any())
+                .As(x => _defaultTypeOfExport(x))
+                .SingleInstance();
+
+            builder.RegisterTypes(types)
+                .Where(
+                    x => !x.GetCustomAttributes(typeof (Shared), true).Any() ||
+                          x.GetCustomAttributes(typeof (NonShared), true).Any())
+                .As(x => _defaultTypeOfExport(x))
+                .InstancePerDependency();
+        }
+    }
+}
