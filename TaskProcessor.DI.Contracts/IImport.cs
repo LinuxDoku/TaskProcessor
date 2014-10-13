f95eb17825c65f16cdf453d9e42608417d99eb90
Martin Lantzsch <martin@linux-doku.de>
2014-10-13 20:56:20 +0200

    fixing latest build errors

--- TaskProcessor.DI.Contracts/IImport.cs
+++ TaskProcessor.DI.Contracts/IImport.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskProcessor.DI.Contracts
+﻿namespace TaskProcessor.DI.Contracts
 {
     public interface IImport
     {
