678d4ef96b956b521690fea36e472ce0e6f2d360
Martin Lantzsch <martin@linux-doku.de>
2014-10-20 23:26:08 +0200

    windows service

---
+++ TaskProcessor.Service/Service.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.ServiceProcess;
+
+namespace TaskProcessor.Service {
+    public class Service : ServiceBase {
+        static void Main() { }
+
+        protected override void OnStart(string[] args) {
+            DI.Container.RegisterAssembly(Assembly.GetAssembly(typeof(IApplication)));
+            DI.Container.GetExport<IApplication>();
+        }
+    }
+}
