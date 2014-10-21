817718fa798686b3e4b5c593fe90bdc16f8cf7b1
Martin Lantzsch <martin@linux-doku.de>
2014-10-21 22:17:32 +0200

    windows service starts app in own thread

--- TaskProcessor.Service/Service.cs
+++ TaskProcessor.Service/Service.cs
@@ -1,11 +1,16 @@
 ﻿using System.Reflection;
 using System.ServiceProcess;
+using System.Threading;
 
 namespace TaskProcessor.Service {
     public class Service : ServiceBase {
         static void Main() { }
 
         protected override void OnStart(string[] args) {
+            new Thread(RunApplication).Start();
+        }
+
+        private void RunApplication() {
             DI.Container.RegisterAssembly(Assembly.GetAssembly(typeof(IApplication)));
             DI.Container.GetExport<IApplication>();
         }
